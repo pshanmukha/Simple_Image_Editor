762e1b80b2195acfc7529393556e633ed65fe5cd
pshanmukha <shanmukhapanyam10@gmail.com>
2022-03-13 19:55:31 +0530

    Initial Commit

---
+++ lib/utils.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+Future<bool> requestPermission(Permission permission) async {
+  if(await permission.isGranted){
+    return true;
+  } else {
+    var result = await permission.request();
+    if(result == PermissionStatus.granted) {
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+}
